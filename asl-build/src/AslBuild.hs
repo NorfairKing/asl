4287badf1f3d3268936689ea694f3379ef3054a7
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-09-21 01:17:10 +0200

    Cleanup and clean rules

--- asl-build/src/AslBuild.hs
+++ asl-build/src/AslBuild.hs
@@ -23,60 +23,107 @@ aslBuild = do
             (Command, flags) <- getArguments first
             withArgs rest $ doTheShake flags
 
+-- Constants
+asl :: String
+asl = "asl"
+
+out :: String
+out = "out"
+
+src :: String
+src = "src"
+
+txt :: String
+txt = "txt"
+
+reports :: String
+reports = "reports"
+
+pdf :: String
+pdf = "pdf"
+
+tex :: String
+tex = "tex"
+
+jar :: String
+jar = "jar"
+
+java :: String
+java = "java"
+
+gradle :: String
+gradle = "gradle"
+
+build :: String
+build = "build"
+
+libs :: String
+libs = "libs"
+
+settings :: String
+settings = "settings"
+
 -- Directories
 outDir :: FilePath
-outDir = "out"
+outDir = out
 
 reportsDir :: FilePath
-reportsDir = "reports"
+reportsDir = reports
 
 codeSrcDir :: FilePath
-codeSrcDir = "asl"
+codeSrcDir = asl
 
 javaSourceDir :: FilePath
-javaSourceDir = codeSrcDir </> "src"
+javaSourceDir = codeSrcDir </> src
 
 -- Extensions
 type Extension = FilePath
 
-pdf :: Extension
-pdf = "pdf"
+pdfExt :: Extension
+pdfExt = pdf
 
-tex :: Extension
-tex = "tex"
+texExt :: Extension
+texExt = tex
 
-jar :: Extension
-jar = "jar"
+txtExt :: Extension
+txtExt = txt
 
-java :: Extension
-java = "java"
+jarExt :: Extension
+jarExt = jar
 
-gradle :: Extension
-gradle = "gradle"
+javaExt :: Extension
+javaExt = java
+
+gradleExt :: Extension
+gradleExt = gradle
 
 doTheShake :: Flags -> IO ()
 doTheShake flags = do
     shakeArgs shakeOptions $
         flip runReaderT flags $ do
+        commitHashRules
         jarRules
         reportRules
+        cleanRules
 
 type AslBuilder = ReaderT Flags Rules
 
 jarRules :: AslBuilder ()
 jarRules = lift $ do
-    let jarFile = "asl" <.> jar
+    let jarFile = asl <.> jar
         jarout = outDir </> jarFile
     want [jarout]
 
-    let jarInGradleBuildDir = codeSrcDir </> "build/libs" </> jarFile
+    let jarInGradleBuildDir = codeSrcDir </> build </> libs </> jarFile
     jarInGradleBuildDir %> \out -> do
-        let buildFile = "build" <.> gradle
-            settingsFile = "settings" <.> gradle
+        let buildFile = codeSrcDir </> build <.> gradle
+            settingsFile = codeSrcDir </> settings <.> gradle
         need [buildFile, settingsFile]
         sourceFiles <- absFilesInDir javaSourceDir ["//*" <.> java]
         need sourceFiles
-        command [Cwd codeSrcDir] "gradle" ["--build-file", buildFile, "jar"]
+        let jarTarget = jar
+            gradleCmd = gradle
+        cmd (Cwd codeSrcDir) gradle jarTarget
 
     jarout `byCopying` jarInGradleBuildDir
 
@@ -88,17 +135,36 @@ reportRules = do
     usingTravis <- asks flagsTravis
     unless usingTravis $ lift $ do
         let reportstubname = "reportstub"
-        let reportStubOut = outDir </> reportstubname <.> pdf
-        let reportStubInBuildDir = reportsDir </> reportstubname <.> pdf
-        let reportstubtex = reportsDir </> reportstubname  <.> tex
+        let reportStubOut = outDir </> reportstubname <.> pdfExt
+        let reportStubInBuildDir = reportsDir </> reportstubname <.> pdfExt
+        let reportstubtex = reportstubname  <.> texExt
+        let reportstubtexInBuildDir = reportsDir </> reportstubtex
         want [reportStubOut]
         reportStubInBuildDir %> \out -> do
-            need [reportstubtex]
-            command [Cwd reportsDir] "latexmk" ["-pdf", reportstubtex]
+            need [reportstubtexInBuildDir]
+            cmd (Cwd reportsDir) "latexmk" "-pdf" reportstubtex
         reportStubOut `byCopying` reportStubInBuildDir
 
 byCopying :: FilePath -> FilePath -> Rules ()
-byCopying to from = to %> \_ -> do
-    need [from]
-    cmd "cp" from to
+byCopying to from = to %> \_ -> copyFileChanged from to
+
+commitHashRules :: AslBuilder ()
+commitHashRules = lift $ do
+    let commithash = "commit"
+        commithashFile = commithash <.> txtExt
+    want [commithashFile]
+    commithashFile %> \out -> do
+        alwaysRerun
+        -- Make the hash as short as possible with --short
+        Stdout hash <- quietly $ cmd "git rev-parse --short=0 --verify HEAD"
+        Stdout dirtyStr <- quietly $ cmd "git ls-files --exclude-standard --others"
+        -- # init to remove newline
+        let contents = init hash ++ if null (dirtyStr :: String) then [] else "-dirty" ++ "\n"
+        writeFileChanged out contents
+
+cleanRules :: AslBuilder ()
+cleanRules = lift $ phony "clean" $ do
+    removeFilesAfter outDir ["//"]
+    removeFilesAfter codeSrcDir ["//build", "//out"]
+    removeFilesAfter reportsDir ["//*.pdf", "//*.aux", "//*.log", "//*.fls", "//*.fdb_latexmk"]
 
