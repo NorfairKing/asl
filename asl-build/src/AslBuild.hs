32d3dd52da77018627d88e008cc1898c397c28e5
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-09-20 20:18:27 +0200

    A runnable java program

--- asl-build/src/AslBuild.hs
+++ asl-build/src/AslBuild.hs
@@ -3,6 +3,22 @@ module AslBuild
     ) where
 
 import           Development.Shake
+import           Development.Shake.FilePath
+
 
 aslBuild :: IO ()
-aslBuild = putStrLn "ASL Build system"
+aslBuild = do
+    putStrLn "ASL Build system"
+    shakeArgs shakeOptions $ do
+        let outdir = "out"
+            jarfile = "asl.jar"
+            jarout = outdir </> jarfile
+        want [jarout]
+        let jarInGradleBuildDir = "asl/build/libs/asl-0.1.jar"
+        jarInGradleBuildDir %> \out -> do
+            let buildFile = "asl/build.gradle"
+            need [buildFile]
+            cmd "gradle" "--build-file" buildFile "jar"
+        jarout %> \out -> do
+            need [jarInGradleBuildDir]
+            cmd "mv" jarInGradleBuildDir out
---
+++ asl/src/eu/cssyd/asl/AslMain.java
@@ -0,0 +1,7 @@
+package eu.cssyd.asl;
+
+public class AslMain {
+    public static void main(String[] args) {
+        System.out.println("ASL");
+    }
+}
