e01ca5e7d67dc874a71bdfbaf7435d7b887ea0e7
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-09-25 11:50:16 +0200

    Cleaned up the baseline experiment setup

--- asl-build/src/AslBuild/RunBaseLine.hs
+++ asl-build/src/AslBuild/RunBaseLine.hs
@@ -2,6 +2,11 @@
 module AslBuild.RunBaseLine where
 
 import           Development.Shake
+import           Development.Shake.FilePath
+
+import           Control.Concurrent
+import           Data.Hashable
+import           System.Directory
 
 import           AslBuild.Memaslap
 import           AslBuild.Memcached
@@ -21,13 +26,42 @@ baselineExperimentRule :: String
 baselineExperimentRule = "baseline-experiment"
 
 logFile :: FilePath
-logFile = "tmp/memaslaplog2.txt"
+logFile = "tmp/baseline_memaslaplog.txt"
 
 memaslapConfigFile :: FilePath
 memaslapConfigFile = "tmp/distribution.txt"
 
 csvOut :: FilePath
 csvOut = "out/baseline_experiment.csv"
+
+wait :: Int -> Action ()
+wait i = liftIO $ threadDelay $ i * 1000 * 1000
+
+data Script
+    = Script
+    { scriptName    :: FilePath
+    , scriptContent :: [String]
+    }
+
+namedScript :: FilePath -> [String] -> Script
+namedScript name contents = Script name $ "#!/bin/bash" : contents
+
+script :: [String] -> Script
+script contents = namedScript (show $ hash contents) contents
+
+tmpMemaslap :: FilePath
+tmpMemaslap = "/tmp/memaslap"
+
+tmpMemaslapCfg :: FilePath
+tmpMemaslapCfg = "/tmp/memaslapcfg"
+
+tmpMemcached :: FilePath
+tmpMemcached = "/tmp/memcached"
+
+startMemcachedScript :: Script
+startMemcachedScript = Script
+    "start-memcached-detached"
+    ["screen -d -m " ++ tmpMemcached]
 
 baselineExperiment :: Rules ()
 baselineExperiment = do
@@ -38,33 +72,43 @@ baselineExperiment = do
         let client = localhost
         let server = localhost
 
-        let tmpMemaslap = "/tmp/memaslap"
-        let tmpMemaslapCfg = "/tmp/memaslapcfg"
-        let tmpMemcached = "/tmp/memcached"
-
+        -- Copy memaslap and its config to the client
         rsyncTo client memaslapBin tmpMemaslap
         rsyncTo client memaslapConfigFile tmpMemaslapCfg
+
+        -- Copy memcached and its config to the server
         rsyncTo server memcachedBin tmpMemcached
 
         -- Start memcached on the server
-        overSsh server ("screen -d -m " ++ tmpMemcached)
+        scriptAt server startMemcachedScript
+
+        -- Wait for the server to get started
+        wait 1
+
+        -- Time to run the experiment for
+        let timeInSeconds = 2
 
         let flags = MemaslapFlags
                 { msServers = [RemoteServerUrl "localhost" 11211]
                 , msThreads = 64
                 , msConcurrency = 64
                 , msOverwrite = 1
-                , msStatFreq = Seconds 2
-                , msTime = Seconds 2
+                , msStatFreq = Seconds timeInSeconds
+                , msTime = Seconds timeInSeconds
                 , msConfigFile = tmpMemaslapCfg
                 }
 
         let remoteLog = "/tmp/memaslaplog"
+        let memaslapScript = script
+                [ unwords
+                $ tmpMemaslap : memaslapArgs flags
+                    ++ [">", remoteLog, "2>&1", "&"]
+                ]
 
-        overSsh client $
-            unwords $
-                tmpMemaslap : memaslapArgs flags
-                ++ [">", remoteLog]
+        scriptAt client memaslapScript
+
+        -- Wait long enough to be sure that memaslap is fully done.
+        wait (timeInSeconds + 5)
 
         -- Make sure no memcached servers are running anymore
         overSsh server $ unwords ["killall", memcachedBinName]
@@ -78,6 +122,21 @@ baselineExperiment = do
             Just parsedLog -> writeFile' csvOut $ parsedLogsCsv [parsedLog]
 
 
+scriptAt :: RemoteLogin -> Script -> Action ()
+scriptAt rl Script{..} = do
+    let path = "/tmp" </> scriptName <.> "bash"
+    let fullScript = unlines scriptContent
+    writeFile' path fullScript
+    -- Make sure it's executable before we copy the script
+    -- rsync will retain the permissions.
+    liftIO $ do
+        p <- getPermissions path
+        setPermissions path (p {executable = True})
+    -- Copy over the script
+    rsyncTo rl path path
+    liftIO $ putStrLn $ "Running on " ++ remoteLoginStr rl ++ ":\n" ++ fullScript
+    -- Run the script
+    overSsh rl path
 
 overSsh :: RemoteLogin -> String -> Action ()
 overSsh rl commandOverSsh =
@@ -86,11 +145,17 @@ overSsh rl commandOverSsh =
 rsyncTo :: RemoteLogin -> FilePath -> FilePath -> Action ()
 rsyncTo rl localThing remoteThing = do
     need [localThing]
-    command [] "rsync" [localThing, remoteLoginStr rl ++ ":" ++ remoteThing]
+    command [] "rsync"
+        [ localThing
+        , remoteLoginStr rl ++ ":" ++ remoteThing
+        ]
 
 rsyncFrom :: RemoteLogin -> FilePath -> FilePath -> Action ()
 rsyncFrom rl remoteThing localThing =
-    command [] "rsync" [remoteLoginStr rl ++ ":" ++ remoteThing, localThing]
+    command [] "rsync"
+        [ remoteLoginStr rl ++ ":" ++ remoteThing
+        , localThing
+        ]
 
 remoteLoginStr :: RemoteLogin -> String
 remoteLoginStr RemoteLogin{..} = remoteUser ++ "@" ++ remoteHost
