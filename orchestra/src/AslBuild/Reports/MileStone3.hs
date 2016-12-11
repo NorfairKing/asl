2289163b1688cda19276f80e66911ec1017b5acb
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-12-11 17:48:44 +0100

    Some nice drawings

--- orchestra/src/AslBuild/Reports/MileStone3.hs
+++ orchestra/src/AslBuild/Reports/MileStone3.hs
@@ -6,11 +6,13 @@ import           AslBuild.Experiments.StabilityTrace
 import           AslBuild.Models.MM1
 
 import           AslBuild.Reports.Common
+import           AslBuild.Reports.MileStone1
 
 report3Rules :: Rules ()
 report3Rules = report 3 texPreAction customRules
   where
     customRules =
         remoteStabilityTrace `useMM1ModelInReport` 3
-    texPreAction =
+    texPreAction = do
         remoteStabilityTrace `dependOnMM1ModelForReport` 3
+        need [architecturePng]
