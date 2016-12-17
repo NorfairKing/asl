5ec4e989d02fd7038d181e584622fd188f6861e3
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-12-17 22:18:24 +0100

    Tried plotting something in section 2

--- analysis/mmm_analysis.r
+++ analysis/mmm_analysis.r
@@ -12,5 +12,28 @@ outFile <- args[3]
 res = read.csv(inFile, header=TRUE, colClasses="numeric")
 print(res)
 
+resp = res$respavgavg
+
 startPng(outFile)
-plot(res$meanResponseTime, res$respavgavg)
+plot(
+    NULL
+  , NULL
+  , type='n'
+  , xlim=c(0,1)
+  , ylim=c(0,max(resp))
+  , xlab="Utilisation rate"
+  , ylab="Response time (microseconds)"
+  )
+
+colors = c("red", "blue", "green")
+
+for (i in (1:nrow(res))) {
+  rhos = seq(from=0, to=0.95, by=0.01)
+  mu = res$serviceRate[i]
+  m = res$nrServers[i]
+  r = (1 / mu) * (1 + (rhos / (m * (1 - rhos)))) * 1000 * 1000
+  
+  c = colors[(i %% length(colors)) + 1]
+  lines(rhos, r, col=c)
+}
+points(res$trafficIntensity, resp)
