4f318e1401206a31e2cb27f00d7413889c22467f
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2016-12-17 17:15:48 +0100

    Started on estimation in my own model

--- orchestra/src/AslBuild/Models/MyModel.hs
+++ orchestra/src/AslBuild/Models/MyModel.hs
@@ -2,21 +2,26 @@ module AslBuild.Models.MyModel where
 
 import           Control.Monad
 import           Control.Monad.IO.Class
+import           Data.List
 
 import           Development.Shake
 import           Development.Shake.FilePath
 
 import           AslBuild.Analysis.Memaslap
+import           AslBuild.Analysis.Trace
 import           AslBuild.Analysis.Types
 import           AslBuild.Analysis.Utils
+import           AslBuild.Client.Types
 import           AslBuild.Constants
 import           AslBuild.Experiment
 import           AslBuild.Experiments.ReplicationEffect
+import           AslBuild.Memaslap.Types
+import           AslBuild.Middle.Types
+import           AslBuild.Middleware.Types
 import           AslBuild.Models.MM1.Types
 import           AslBuild.Models.MMm.Types
 import           AslBuild.Models.MyModel.Types
 import           AslBuild.Models.Utils
-import           AslBuild.Reports.Common
 import           AslBuild.Utils
 
 myModelRule :: String
@@ -54,8 +59,10 @@ myModelRuleFor ecf = experimentTarget ecf ++ "-my-model"
 
 myModelRulesFor :: ExperimentConfig a => a -> Rules (Maybe String)
 myModelRulesFor ecf = onlyIfResultsExist ecf $ do
+    estr <- myEstimationRulesFor ecf
+
     let rule = myModelRuleFor ecf
-    rule ~> need []
+    rule ~> need [estr]
     pure rule
 
 readMyModelFile :: MonadIO m => FilePath -> m MyModel
@@ -83,7 +90,62 @@ myEstimationRulesFor ecf = do
 
 estimateMyModel :: ExperimentConfig a => a -> [FilePath] -> Action MyModel
 estimateMyModel ecf slocs = do
-    let mm1 = MM1Model 0 0
-    let mmm = MMmModel 0 0 0
-    pure $ MyModel mm1 mmm mm1 mm1
+    ers <- mapM readResultsSummary slocs
+    mrfs <- case mapM merMiddleResultsFile ers of
+        Nothing  -> fail "Need middles for my model."
+        Just mes -> pure mes
+
+    let combinedResultsFile = combinedClientRepsetResultsFile ecf slocs
+    let combinedAvgDursFile = combinedAvgDurationFile ecf mrfs
+    let neededFiles = [combinedResultsFile, combinedAvgDursFile]
+    need neededFiles
+    putLoud $ unwords ["Making my model from:", show neededFiles]
+
+    setup <- readExperimentSetupForSummary $ head ers
+    cres <- readCombinedClientsResults combinedResultsFile
+    avgDurs <- readCombinedAvgDursFile combinedAvgDursFile
+
+    (middleSetup, serverSetups) <- case backendSetup setup of
+        Left _    -> fail "need middlesetup for my model."
+        Right tup -> pure tup
+
+    let unMiddleTime = (/ (10 ** 9))
+        -- unMemaTime = (/ (10 ** 6))
+
+    let parsingTime = avgAvgs (untilParsedTime avgDurs)
+        enqueuingTime = avgAvgs (untilEnqueuedTime avgDurs)
+
+    let nrSers = genericLength serverSetups
+        setProp = setProportion $ msConfig $ cMemaslapSettings $ head $ clientSetups setup
+        getProp = 1 - setProp
+        nrReadThreads = mwNrThreads $ mMiddlewareFlags middleSetup
+
+    -- Arrival rate at acceptor = average throughput
+    let accλ = avgAvgs $ avgBothResults $ avgTpsResults cres
+    -- Service time at acceptor = average parsing + hashing + enqueueing
+    -- Service rate is inverse of that.
+    let accμ = (1/) $ unMiddleTime $ parsingTime + enqueuingTime
+
+
+    let serverWorkerλ = accλ / nrSers
+
+    -- Arrival rate at get workers
+    let getλ = getProp * serverWorkerλ
+    let getμ = 0
+    let getm = nrReadThreads
+
+    -- Arrival rate at set workers
+    let setλ = setProp * serverWorkerλ
+    let setμ = 0
+
+    -- Arrival rate at servers
+    let serλ = serverWorkerλ
+    let serμ = 0
+
+    let accModel_ = MM1Model accλ accμ
+        getModel_ = MMmModel getλ getμ getm
+        setModel_ = MM1Model setλ setμ
+        serModel_ = MM1Model serλ serμ
+
+    pure $ MyModel accModel_ getModel_ setModel_ serModel_
 
